2f59b4944778653a5811b5743da57f3b34cb2062
ddany <duminydaniel@gmail.com>
2018-01-23 19:35:20 +0530

    Update database.php

--- _database/database.php
+++ _database/database.php
@@ -1,8 +1,8 @@
 <?php
-    $hostname = "localhost";
-    $user = "root";
-    $password = "mysql";
-    $database = "forum";
+    $hostname = "db4free.net";
+    $user = "myforum101";
+    $password = "rootroot";
+    $database = "forum101";
     $prefix = "";
     $database=mysqli_connect($hostname,$user,$password,$database);
 ?>
