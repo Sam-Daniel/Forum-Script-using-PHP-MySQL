b05cda20b23bfe51e6b415f08b7cd61ec0a6a403
ddany <duminydaniel@gmail.com>
2018-01-25 13:09:04 +0530

    Update database.php

--- _database/database.php
+++ _database/database.php
@@ -1,8 +1,8 @@
 <?php
-    $hostname = "localhost";
-    $user = "root";
-    $password = "mysql";
-    $database = "forum";
+    $hostname = "sql12.freesqldatabase.com";
+    $user = "sql12217713";
+    $password = "qEKJRItGpl";
+    $database = "sql12217713";
     $prefix = "";
     $database=mysqli_connect($hostname,$user,$password,$database);
 ?>
